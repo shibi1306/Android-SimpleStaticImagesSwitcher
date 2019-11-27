705c9f257b71fb2bc19aa5cc7716bc98525093d6
Shibi S.Kumar <dev.shibi1306@gmail.com>
2019-11-28 01:29:13 +0530

    Fixed: App crashing when the images array is empty.
    Added: Button disabled when there are no images or only one image in the images array.
    
    Reworked: Separate initializers for variables and widgets. Button instance initialization added.
    
    Reworked: Grey colored initial ImageView instead of an image.
    
    Removed: index visibility in Log.

--- app/src/main/java/com/example/loadingimages/MainActivity.java
+++ app/src/main/java/com/example/loadingimages/MainActivity.java
@@ -4,6 +4,7 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
 import android.view.View;
+import android.widget.Button;
 import android.widget.ImageView;
 import android.util.Log;
 
@@ -13,11 +14,12 @@ public class MainActivity extends AppCompatActivity {
     int images[]; // will hold the ids of all the images. Even Array class can be implemented for storing the imageIds.
     int n; // count of images[]
 
+    // Widgets
+    ImageView displayedImageView;
+    Button button;
+
     public void switchImage(View button){
         Log.i("Action", "Button tapped");
-
-        // Instance of the image view to change displaying image
-        ImageView displayedImageView = (ImageView) findViewById(R.id.imageView);
 
         // index gets incremented by one. Once last image in the array is displayed, index will go back to first image
         index = (index + 1) % n;
@@ -28,17 +30,40 @@ public class MainActivity extends AppCompatActivity {
         // setting the image view to display the extracted image
         displayedImageView.setImageResource(imageId);
 
-        Log.i("Event", "Image switched to dog " + Integer.toString(index + 1));
+        Log.i("Event", "Image switched");
+    }
+
+    private void variablesInitialization(){
+        index = 0;
+        // Images that are getting displayed...
+        images = new int[] {R.drawable.dog1, R.drawable.dog2,
+                R.drawable.dog3, R.drawable.dog4, R.drawable.dog5, R.drawable.dog6};
+
+        n = images.length;
+    }
+
+    private void widgetInitialization(){
+        // Instance of the image view to change displaying image
+        displayedImageView = (ImageView) findViewById(R.id.imageView);
+
+        // Instance of switching image button
+        button = (Button) findViewById(R.id.button);
     }
 
     // Initialization of the class members....
     private void initialization(){
-        index = 0;
 
-        // Images that are getting displayed... If new
-        images = new int[] {R.drawable.dog1, R.drawable.dog2,
-                R.drawable.dog3, R.drawable.dog4, R.drawable.dog5, R.drawable.dog6};
-        n = images.length;
+        variablesInitialization();
+        widgetInitialization();
+
+// Set image only when the image array is non-empty.
+        if (n > 0){
+            displayedImageView.setImageResource(images[index]);
+        }
+// Disable the button when there are no images or only one image.
+        if (n <= 1){
+            button.setEnabled(false);
+        }
     }
 
     @Override
